944959580a9e07027db797573fe8517cfc763ccf
Joaquín Viretti <joaquin.viretti@gmail.com>
2026-04-30 17:39:30 -0300

    Desarrollo lógica principal

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+# main.py
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from app.puntos import calcular_puntos
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    print("=== Cálculo de puntos Pasaporte Despegar ===")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        monto = float(input("Ingrese el monto de la compra: "))
+        tarjeta = input("Ingrese tipo de tarjeta (gold/platinum/black): ").lower()
+
+        puntos = calcular_puntos(monto, tarjeta)
+
+        print(f"Puntos obtenidos: {puntos}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
